55adc859180c133ebefec821ee1b34b7c5462129
Angeline Mary Marchella <angeline030303@gmail.com>
2024-08-22 17:52:06 +0700

    feat: kuisioner awal page

---
+++ app/kuisioner/options.ts
@@ -0,0 +1,110 @@
+export const agreements = [
+    {
+        id: "agreement-1",
+        label: "Memberikan informasi data diri, jenjang pendidikan, dan informasi lainnya yang diperlukan peneliti secara jujur. Pihak BEEXPERT berjanji untuk menjaga kerahasiaan identitas pengguna."
+    },
+    {
+        id: "agreement-2",
+        label: "Menjawab pertanyaan-pertanyaan yang diajukan dengan serius dan sesuai dengan pengalaman pengguna."
+    },
+    {
+        id: "agreement-3",
+        label: "Mengerjakan soal yang dijadikan sebagai bahan uji coba dengan sungguh-sungguh dan memanfaatkan bantuan chatbot BEEXPERT dengan bijak."
+    },
+]
+
+export const question5 = [
+    {
+        id: "question5-1",
+        label: "Membantu memberikan solusi terhadap suatu pertanyaan."
+    },
+    {
+        id: "question5-2",
+        label: "Menjelaskan konsep-konsep yang kurang dipahami."
+    },
+    {
+        id: "question5-3",
+        label: "Memberikan ide dan masukan."
+    },
+    {
+        id: "question5-4",
+        label: "Membantu mengerjakan tugas."
+    },
+    {
+        id: "question5-5",
+        label: "Menyusun materi presentasi atau pengajaran."
+    },
+    {
+        id: "question5-6",
+        label: "Membuat materi dan latihan soal."
+    },
+    {
+        id: "question5-7",
+        label: "Lainnya."
+    }
+]
+
+export const question6 = [
+    {
+        id: "question6-1",
+        label: "Pemberian jawaban yang cukup akurat."
+    },
+    {
+        id: "question6-2",
+        label: "Pemberian jawaban yang instan."
+    },
+    {
+        id: "question6-3",
+        label: "Dapat memahami input gambar dan teks dengan baik."
+    },
+    {
+        id: "question6-4",
+        label: "Lainnya."
+    }
+]
+
+export const question7 = [
+    {
+        id: "question7-1",
+        label: "Jawaban yang kadang ambigu."
+    },
+    {
+        id: "question7-2",
+        label: "Jawaban yang kurang detail."
+    },
+    {
+        id: "question7-3",
+        label: "Jawaban yang keliru."
+    },
+    {
+        id: "question7-4",
+        label: "Keterbatasan dalam berinteraksi."
+    },
+    {
+        id: "question7-5",
+        label: "Lainnya."
+    }
+]
+
+export const question9 = [
+    {
+        id: "question9-1",
+        label: "Chatbot yang didesain untuk lebih berfokus pada pemberian stimulus dibandingkan jawaban secara eksplisit."
+    },
+    {
+        id: "question9-2",
+        label: "Chatbot yang dapat memahami dan menyesuakan responsnya sesuai dengan tujuan pembelajaran yang ditetapkan."
+    },
+    {
+        id: "question9-3",
+        label: "Chatbot yang dapat berinteraksi secara aktif, seperti melalui suara dan coretan."
+    },
+    {
+        id: "question9-4",
+        label: "Chatbot yang dapat secara otomatis menguji pemahaman siswa, seperti dengan memberikan pop quiz."
+    },
+    {
+        id: "question9-5",
+        label: "Lainnya."
+    }
+]
