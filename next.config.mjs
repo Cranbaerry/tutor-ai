55039c2356573d71e110c2dbc76dbd847317a6b7
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-10-02 08:44:39 +0000

    adj: add codespaces config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   // experimental: {
   //   taint: true,
   // },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000', // localhost
+        // 'supreme-journey-69p7qp7x45r346jq-3000.app.github.dev', // Codespaces
+      ],
+    },
+  },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
     return config;
