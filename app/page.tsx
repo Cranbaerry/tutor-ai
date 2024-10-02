1a86b52fff6104bc9bc197964888b7fa09fe031a
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-10-02 20:16:45 +0000

    adj: server side render on auth page

--- app/page.tsx
+++ app/page.tsx
@@ -1,117 +1,52 @@
-'use client';
-
-import { useState, useEffect, Suspense } from "react";
-import Image from "next/legacy/image";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "@/components/ui/use-auth-form";
 import { getUserData } from "@/lib/utils";
-import { useRouter, useSearchParams } from "next/navigation";
-import { toast } from "sonner";
+import AuthPageContent from "@/components/ui/auth-page-content";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from 'next/navigation'
+import Image from "next/legacy/image";
 
-function AuthPageContent() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [formMode, setFormMode] = useState<'SignUp' | 'SignIn'>('SignIn');
-
-  const toggleFormMode = () => {
-    setFormMode((prevMode) => (prevMode === 'SignUp' ? 'SignIn' : 'SignUp'));
-  };
-
-  useEffect(() => {
-    async function checkUser() {
-      const data = await getUserData();
-      if (data) {
-        console.log(data);
-        toast.success("Successfully logged in", { description: `Welcome back, ${data?.email ?? "Guest"}` });
-        router.push("/kuisioner");
-      }
-    }
-
-    async function checkAuthCodeError() {
-      const isAuthCodeError = searchParams.get('auth-code-error') !== null;
-      await new Promise(resolve => setTimeout(resolve, 100)); // wait for toast to be ready
-      if (isAuthCodeError) {
-        console.log('error????');
-        toast.error("Error", { description: "Kode autentikasi tidak valid." });
-      }
-    }
-
-    checkAuthCodeError();
-    checkUser();
-  }, [router, searchParams]);
+async function AuthenticationPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+  const supabase = createClient();
+  const userData = await getUserData(supabase);
+  const isAuthCodeError = 'auth-code-error' in searchParams;
+  if (userData) redirect('/kuisioner');
 
   return (
-    <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 h-screen">
-        <Link
-          href="#"
-          onClick={toggleFormMode}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          {formMode === 'SignUp' ? 'Masuk' : 'Daftar'}
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0">
-            <Image
-              src="/students.jpg"
-              alt="Picture of students"
-              objectFit="cover"
-              layout="fill"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50"></div>
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-base">
-                Platform bimbingan belajar berbasis AI yang dirancang untuk siswa SMA yang memungkinkan siswa untuk memasukkan pertanyaan melalui suara dan gambar secara langsung.
-              </p>
-              <footer className="text-xs italic">
-                Bagian dari penelitian &ldquo;Chatbot System with Retrieval Augmented Generation for Enhanced Self-learning Experience&rdquo; oleh{" "}
-                <a href="https://www.linkedin.com/in/angeline-mary-marchella/" target="_blank">
-                  Angeline Mary Marchella
-                </a>
-                ,{" "}
-                <a href="https://www.linkedin.com/in/naufal-h/" target="_blank">
-                  Naufal Hardiansyah
-                </a>
-                , dan{" "}
-                <a href="https://www.linkedin.com/in/nathaniel-candra-b21288206/" target="_blank">
-                  Nathaniel Chandra
-                </a>.
-              </footer>
-            </blockquote>
-          </div>
+    <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 h-screen">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0">
+          <Image
+            src="/students.jpg"
+            alt="Picture of students"
+            objectFit="cover"
+            layout="fill"
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50"></div>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <UserAuthForm formMode={formMode} />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              Dengan memakai platform ini, anda setuju{" "}
-              <Link href="#" className="underline underline-offset-4 hover:text-primary">
-                Terms of Service
-              </Link>{" "}
-              dan{" "}
-              <Link href="#" className="underline underline-offset-4 hover:text-primary">
-                Privacy Policy
-              </Link>
-              .
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-base">
+              Platform bimbingan belajar berbasis AI yang dirancang untuk siswa SMA yang memungkinkan siswa untuk memasukkan pertanyaan melalui suara dan gambar secara langsung.
             </p>
-          </div>
+            <footer className="text-xs italic">
+              Bagian dari penelitian &ldquo;Chatbot System with Retrieval Augmented Generation for Enhanced Self-learning Experience&rdquo; oleh{" "}
+              <a href="https://www.linkedin.com/in/angeline-mary-marchella/" target="_blank">
+                Angeline Mary Marchella
+              </a>
+              ,{" "}
+              <a href="https://www.linkedin.com/in/naufal-h/" target="_blank">
+                Naufal Hardiansyah
+              </a>
+              , dan{" "}
+              <a href="https://www.linkedin.com/in/nathaniel-candra-b21288206/" target="_blank">
+                Nathaniel Chandra
+              </a>.
+            </footer>
+          </blockquote>
         </div>
       </div>
-    </>
+      <AuthPageContent isAuthCodeError={isAuthCodeError} />
+    </div>
   );
 }
 
-export default function AuthenticationPage() {
-  return (
-    <Suspense>
-      <AuthPageContent />
-    </Suspense>
-  );
-}
+export default AuthenticationPage;
---
+++ components/ui/auth-page-content.tsx
@@ -0,0 +1,210 @@
+'use client';
+
+import { useState, useEffect, useCallback } from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { toast } from "sonner";
+import Image from "next/legacy/image";
+import { useRouter } from "next/navigation";
+import { signInWithPassword, signInWithOAuth, signUp } from "@/app/actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Icons } from "@/components/ui/icons";
+
+function AuthPageContent({ isAuthCodeError }: { isAuthCodeError: boolean }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [formMode, setFormMode] = useState<'SignUp' | 'SignIn'>('SignIn');
+  const [currentFormMode, setCurrentFormMode] = useState(formMode);
+
+  const toggleFormMode = () => {
+    setFormMode((prevMode) => (prevMode === 'SignUp' ? 'SignIn' : 'SignUp'));
+  };
+
+  useEffect(() => {
+    handleCheckAuthCodeError();
+  }, []);
+
+  // Update form mode with animation
+  useEffect(() => {
+    if (formMode !== currentFormMode) {
+      setIsLoading(true);
+      const timeoutId = setTimeout(() => {
+        setCurrentFormMode(formMode);
+        setIsLoading(false);
+      }, 300); // Animation duration
+      return () => clearTimeout(timeoutId);
+    }
+  }, [formMode, currentFormMode]);
+
+  const handleCheckAuthCodeError = async () => {
+    if (isAuthCodeError) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      toast.error("Error", { description: "Kode autentikasi tidak valid." });
+    }
+  }
+
+  const handleSignUp = useCallback(async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const { data, error } = await signUp({ email, password });
+    setIsLoading(false);
+
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    toast.success("Successfully signed up", { description: `Hello ${data?.user?.email ?? "Guest"}` });
+    router.push("/kuisioner");
+  }, [router]);
+
+  const handleSignIn = useCallback(async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const { data, error } = await signInWithPassword({ email, password });
+
+    if (error) {
+      toast.error(error);
+      setIsLoading(false);
+      return;
+    }
+
+    toast.success("Successfully logged in", { description: `Hello ${data?.user?.email ?? "Guest"}` });
+    router.push("/kuisioner");
+  }, [router]);
+
+  const handleOAuthLogin = useCallback(async () => {
+    setIsLoading(true);
+    const { data, error } = await signInWithOAuth();
+
+    if (error) {
+      toast.error(error);
+      setIsLoading(false);
+      return;
+    }
+
+    if (!data?.url) {
+      toast.error("Oops! Something went wrong, please try again later");
+      setIsLoading(false);
+      return;
+    }
+
+    toast.info("Redirecting", { description: "You are now being redirected.." });
+    router.push(data.url);
+  }, [router]);
+
+  return (
+    <div className="lg:p-8">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <Link
+          href="#"
+          onClick={toggleFormMode}
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          {formMode === 'SignUp' ? 'Masuk' : 'Daftar'}
+        </Link>
+        <>
+          <div className="flex flex-col items-center justify-center space-y-2">
+            <Image src="/beexpert-logo.svg" alt="BEEXPERT Logo" width={80} height={82.5} priority />
+            <Image src="/beexpert-name.svg" alt="BEEXPERT" width={170} height={19} priority />
+          </div>
+
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              {currentFormMode === "SignUp" ? "Buat akun baru" : "Login dengan email"}
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              {currentFormMode === "SignUp" ? "Masukkan email untuk membuat akun" : "Masukkan kredential untuk melanjutkan"}
+            </p>
+          </div>
+
+          <div
+            className={cn(
+              "grid gap-6 transition-opacity duration-300 ease-in-out",
+              isLoading ? "opacity-50" : "opacity-100"
+            )}
+          >
+            <form onSubmit={currentFormMode === "SignUp" ? handleSignUp : handleSignIn}>
+              <div className="grid gap-2">
+                <div className="grid gap-2">
+                  <Label className="sr-only" htmlFor="email">
+                    Email
+                  </Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    placeholder="name@example.com"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  />
+                  <Input
+                    id="password"
+                    name="password"
+                    placeholder="Kata Sandi"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="password"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                  />
+                </div>
+                <Button disabled={isLoading}>
+                  {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+                  {currentFormMode === "SignUp" ? "Daftar" : "Masuk"}
+                </Button>
+              </div>
+            </form>
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">Atau masuk dengan</span>
+              </div>
+            </div>
+
+            <Button variant="outline" type="button" disabled={isLoading} onClick={handleOAuthLogin}>
+              {isLoading ? <Icons.spinner className="mr-2 h-4 w-4 animate-spin" /> : <Icons.google className="mr-1 h-4 w-4" />}{" "}
+              Google
+            </Button>
+          </div>
+        </>
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          Dengan memakai platform ini, anda setuju{" "}
+          <Link href="#" className="underline underline-offset-4 hover:text-primary">
+            Terms of Service
+          </Link>{" "}
+          dan{" "}
+          <Link href="#" className="underline underline-offset-4 hover:text-primary">
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPageContent;
