8174a7a9110756a03f47ef39fc41f71456682d9b
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-10-08 05:54:41 +0000

    adj: remove form trigger

--- components/ui/questionnaire-section-user.tsx
+++ components/ui/questionnaire-section-user.tsx
@@ -30,11 +30,10 @@ export default function DataDiriSection() {
       const user = await getUserData();
       if (user != null) {
         form.setValue("fullName", user.user_metadata.full_name ?? "");
-        form.setValue("email", user.email ?? "");
       }
     };
     fetchData();
-  }, [form]);
+  }, []);
 
   return (
     <>
@@ -58,7 +57,6 @@ export default function DataDiriSection() {
                     {...field}
                     onChange={(e) => {
                       field.onChange(e.target.value);
-                      form.trigger("fullName");
                     }}
                     required
                   />
@@ -98,7 +96,6 @@ export default function DataDiriSection() {
                   <RadioGroup
                     onValueChange={(value) => {
                       field.onChange(value);
-                      form.trigger("gender");
                     }}
                     defaultValue={field.value}
                     className="flex flex-row space-y-1"
@@ -134,7 +131,6 @@ export default function DataDiriSection() {
                 <Select
                   onValueChange={(value) => {
                     field.onChange(value);
-                    form.trigger("profession");
                   }}
                   defaultValue={field.value}
                 >
@@ -167,7 +163,6 @@ export default function DataDiriSection() {
                 <Select
                   onValueChange={(value) => {
                     field.onChange(value);
-                    form.trigger("educationLevel");
                   }}
                   defaultValue={field.value}
                 >
@@ -217,7 +212,6 @@ export default function DataDiriSection() {
                     {...field}
                     onChange={(e) => {
                       field.onChange(e.target.value);
-                      form.trigger("school");
                     }}
                   />
                 </FormControl>
